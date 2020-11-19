875c93da9006fbbfb8ea56bf8600764504f1c0bc
Stepan Pribytko <49967730+stepanpr@users.noreply.github.com>
2020-11-20 01:06:25 +0300

    Delete start.sh

--- ft_services/srcs/nginx/start.sh
+++
@@ -1,8 +0,0 @@
-mkdir /etc/telegraf
-cp /etc/telegraf.conf /etc/telegraf/telegraf.conf
-sed -i "s~# database = \"telegraf\"~database = \"nginx\"~g" /etc/telegraf/telegraf.conf
-sed -i "s~# urls = \[\"unix:///var/run/influxdb.sock\"\]~urls = \[\"http://influxdb:8086\"\]~g" /etc/telegraf/telegraf.conf
-telegraf &
-
-/usr/sbin/sshd
-nginx -g "daemon off;"
