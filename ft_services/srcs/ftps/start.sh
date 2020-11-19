0d90be9dd4ee48bb21790344917247561a0bbe00
Stepan Pribytko <49967730+stepanpr@users.noreply.github.com>
2020-11-20 01:04:23 +0300

    Delete start.sh

--- ft_services/srcs/ftps/start.sh
+++
@@ -1,8 +0,0 @@
-mkdir /etc/telegraf
-cp /etc/telegraf.conf /etc/telegraf/telegraf.conf
-sed -i "s~# database = \"telegraf\"~database = \"ftps\"~g" /etc/telegraf/telegraf.conf
-sed -i "s~# urls = \[\"unix:///var/run/influxdb.sock\"\]~urls = \[\"http://influxdb:8086\"\]~g" /etc/telegraf/telegraf.conf
-telegraf &
-
-echo "emabel@student.21-school.ru" > /home/admin/ftps.txt
-/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
